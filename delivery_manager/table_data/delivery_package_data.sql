9d261bd045e567c6bea4ca330ea73032454ec07c
Péntek Dávid <pd01@hotmail.hu>
2022-01-12 14:28:11 +0100

    Update delivery_package_data.sql

--- delivery_manager/table_data/delivery_package_data.sql
+++ delivery_manager/table_data/delivery_package_data.sql
@@ -1,89 +1,89 @@
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 0);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(0, 0, 0);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 1);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(1, 0, 1);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 2);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(2, 0, 2);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 3);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(3, 0, 3);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 4);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(4, 0, 4);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 5);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(5, 0, 5);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 6);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(6, 0, 6);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 7);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(7, 0, 7);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(0, 8);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(8, 0, 8);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 9);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(9, 1, 9);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 10);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(10, 1, 10);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 11);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(11, 1, 11);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 12);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(12, 1, 12);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 13);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(13, 1, 13);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 14);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(14, 1, 14);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 15);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(15, 1, 15);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 16);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(16, 1, 16);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 17);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(17, 1, 17);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 18);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(18, 1, 18);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 19);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(19, 1, 19);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(1, 20);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(20, 1, 20);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 21);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(21, 2, 21);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 22);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(22, 2, 22);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 23);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(23, 2, 23);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 24);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(24, 2, 24);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 25);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(25, 2, 25);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 26);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(26, 2, 26);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 27);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(27, 2, 27);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 28);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(28, 2, 28);
 
-INSERT INTO delivery_package(delivery_id, package_id)
-VALUES(2, 29);
+INSERT INTO delivery_package(delivery_package_id, delivery_id, package_id)
+VALUES(29, 2, 29);
